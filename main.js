a61e8b05c051be6c1a637c0ecd36e355f3c0705a
techmahendran <mahendran361999@gmail.com>
2021-03-20 20:08:12 +0530

    delete read.md

--- main.js
+++ main.js
@@ -10,28 +10,28 @@ const sendbtn = document.getElementById('send')
 });
 
 
-// function sum(){
-//     let box = parseFloat(document.getElementById("box").value);
-//     let box2 = parseFloat(document.getElementById("box2").value);
-//     document.getElementById("text").value = box + box2;
+// // function sum(){
+// //     let box = parseFloat(document.getElementById("box").value);
+// //     let box2 = parseFloat(document.getElementById("box2").value);
+// //     document.getElementById("text").value = box + box2;
+// // }
+
+// const firstName = document.getElementById('box');
+
+// const box2 = document.getElementById('box2');
+
+// const box3 = document.getElementById('box3');
+
+// const text = document.getElementById('text');
+
+// function multiply(){
+//     let n1 = document.getElementById("n1").value;
+//     let n2 = document.getElementById("n2").value;
+//     document.getElementById("result").innerHTML = n1 * n2;
 // }
 
-const firstName = document.getElementById('box');
-
-const box2 = document.getElementById('box2');
-
-const box3 = document.getElementById('box3');
-
-const text = document.getElementById('text');
-
-function multiply(){
-    let n1 = document.getElementById("n1").value;
-    let n2 = document.getElementById("n2").value;
-    document.getElementById("result").innerHTML = n1 * n2;
-}
-
-function divide(){
-    let n1 = document.getElementById("n1").value;
-    let n2 = document.getElementById("n2").value;
-    document.getElementById("result").innerHTML = n1 / n2;
-}
+// function divide(){
+//     let n1 = document.getElementById("n1").value;
+//     let n2 = document.getElementById("n2").value;
+//     document.getElementById("result").innerHTML = n1 / n2;
+// }
